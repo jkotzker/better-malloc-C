e678d52c953bace1b85374cdd8d7218b6a763ae5
Joseph Kotzker <joeykotzker@gmail.com>
2015-10-19 16:13:32 -0400

    Implemented some additional testing for my_malloc and my_free

--- mallocme.c
+++ mallocme.c
@@ -12,32 +12,51 @@
 #define free( x )       my_free( x, 0, __FILE__, __LINE__ )
 
 typedef struct mystruct {
-    int i;
-    char c;
+    float i1;
+    float i2;
 } s;
 
 int main(int argc, char **argv) {
     if(argc != 3)
     {
-        printf("ERROR: Expected input in form of 2 numbers.\n");
+        printf("ERROR: Expected input in form of 2 floating point numbers.\n");
         return 1;
     }
     else
     {
         char * a = argv[1];
         char * b = argv[2];
-        int curr = *a - '0';
+        float curr1 = *a - '0';
+        float curr2 = *b - '0';
         printf("Allocating memory for a new struct...\n");
         s * mynew = (s *) malloc(sizeof(s));
         if( mynew != NULL )
         {
-            printf("Malloc succeeded, added data...\n");
-            mynew->i = curr;
-            mynew->c = *b;
-            printf("Data added.\n");
+            printf("Malloc succeeded, adding data...\n");
+            mynew->i1 = curr1;
+            mynew->i2 = curr2;
+            printf("Data added. Trying to access...\n");
+            printf("First datum: %f\n", mynew->i1);
+            printf("Second datum: %f\n", mynew->i2);
         }
         printf("Freeing struct...\n");
         free(mynew);
+
+        printf("Now, let's test some errors...\n");
+
+        printf("First error test...\n");
+        free(mynew);
+
+        printf("Seconf error test...\n");
+        s * mynon;
+        free(mynon);
+
+        char c = 'd';
+        char * cptr = &c;
+        free(cptr);
+
+
         printf("Exiting...\n");
+        return 0;
     }
 }
